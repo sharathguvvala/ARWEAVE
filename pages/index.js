55af6fb583828c6e56daca1011d51112bd60c400
sharathguvvala <guvvalasharath@gmail.com>
2022-08-26 10:58:11 +0530

    props

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import { useContext, useState } from 'react'
 import { MainContext } from '../context'
 
 export default function Home() {
+  const [file, setFile] = useState()
+  const [image, setImage] = useState()
+  const [URI, setURI] = useState()
+  const [amount, setAmount] = useState()
+  const { bundlrInstance, initialiseBundlr, balance, fetchBalance } = useContext(MainContext)
   return (
     
   )
