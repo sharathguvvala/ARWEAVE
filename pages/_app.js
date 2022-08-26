1a2c2755fcd883d138b5bc06ad937fbfbc7d00be
sharathguvvala <guvvalasharath@gmail.com>
2022-08-26 10:52:31 +0530

    bundlr

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
+import { WebBundlr } from "@bundlr-network/client"
+import { MainContext } from '../context'
+import { useState, useRef } from 'react'
+import { providers, utils } from 'ethers'
 
 function MyApp({ Component, pageProps }) {
+  const [bundlrInstance, setBundlrInstance] = useState()
+  const [balance, setBalance] = useState()
+  const bundlrRef = useRef()
   return <Component {...pageProps} />
 }
 
