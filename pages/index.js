0c6962f96c488340ad8baa21eeba1ac306097814
sharathguvvala <guvvalasharath@gmail.com>
2022-08-26 10:57:05 +0530

    removed styling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import BigNumber from 'bignumber.js'
 import { useContext, useState } from 'react'
 import { MainContext } from '../context'
