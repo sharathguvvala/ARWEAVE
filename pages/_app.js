9e546aee259e6425019a561a5fc661f90b62d704
sharathguvvala <guvvalasharath@gmail.com>
2022-08-26 10:53:46 +0530

    fetch balance

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,27 @@ function MyApp({ Component, pageProps }) {
   const [bundlrInstance, setBundlrInstance] = useState()
   const [balance, setBalance] = useState()
   const bundlrRef = useRef()
+
+  async function initialiseBundlr() {
+    await window.ethereum.enable()
+  
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready()
+  
+    const bundlr = new WebBundlr("https://node1.bundlr.network", "matic", provider)
+    await bundlr.ready()
+    
+    setBundlrInstance(bundlr)
+    bundlrRef.current = bundlr
+    fetchBalance()
+  }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance()
+    console.log('bal: ', utils.formatEther(bal.toString()))
+    setBalance(utils.formatEther(bal.toString()))
+  }
+
   return <Component {...pageProps} />
 }
 
